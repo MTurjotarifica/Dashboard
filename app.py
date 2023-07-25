3c728cd2f84ec4dc3f2af666404c203646407c4b
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-07-26 06:06:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import pymysql
 import mysql.connector as mysql
 import numpy as np
@@ -34,7 +34,6 @@ connection.close()
 df_raw.date = pd.to_datetime(df_raw['date'])
 
 text = "vodafone"
-texts = ["vodafone", "o2", "telekom", "1und1"]
 init_date = "2022-12-01"
 index_date = "2022-12-01"
 output_type = "png"
@@ -108,145 +107,138 @@ params = {'key': f'{text.lower()}',
 # function that produces and saves the vis
 
 
-def single(texts, key, geo, cat, startdate, index, indexdate, font_use, out_type):
+def single(key, geo, cat, startdate, index, indexdate, font_use, out_type):
+    df_key = df_raw[(df_raw.keyword == f'{params.get("key")}')
+                    & (df_raw.country == f'{params.get("geo")}')
+                    & (df_raw.gt_category == int(f'{params.get("cat")}'))]
+    if params.get("index") == True:
+        df_key = add_indexing(df_key, 'vl_value', f'{params.get("indexdate")}')
+        var_new = 'vl_value_index'
+    else:
+        var_new = 'vl_value'
+        # running the functions we created to create moving average, smoother
+    df_key = add_ma(df_key, var_new, 14)
+    df_key = add_smoother(df_key, var_new, 0.02)
+
+    df = df_key[df_key.date >= f'{params["startdate"]}']
+
     fig = go.Figure()
-
-    for text in texts:
-        # df_key = df_raw[(df_raw.keyword == text.lower()) & (df_raw.country == geo) & (df_raw.gt_category == int(cat))]
-
-        df_key = df_raw[(df_raw.keyword == text.lower()) & (
-            df_raw.country == geo) & (df_raw.gt_category == int(cat))]
-
-        if index:
-            df_key = add_indexing(df_key, 'vl_value', indexdate)
-            var_new = 'vl_value_index'
-        else:
-            var_new = 'vl_value'
-            # running the functions we created to create moving average, smoother
-        df_key = add_ma(df_key, var_new, 14)
-        df_key = add_smoother(df_key, var_new, 0.02)
-
-        df = df_key[df_key.date >= f'{params["startdate"]}']
-
-        fig = go.Figure()
-
-        fig.add_trace(
-            go.Scatter(
-                x=df.date,
-                y=df[var_new],
-                name=f'{text} - Original',
-                mode='lines',
-                opacity=0.3,
-                line=dict(color='#024D83',
-                          width=4),
-                showlegend=True
-            ))
-        # creating the trendline values
-        df_trend = df[['date', var_new]]  # i.e we need date and vl_value
-        # dropping 0 because trendlines can't cope without numeric values
-        df_trend0 = df_trend.dropna()
-        x_sub = df_trend0.date
-        y_sub = df_trend0[var_new]
-        # transforming dates to numeric values, necessary for polynomial fitting
-        x_sub_num = mdates.date2num(x_sub)
-        z_sub = np.polyfit(x_sub_num, y_sub, 1)  # polynomial fitting
-        p_sub = np.poly1d(z_sub)
-        # adding the trendline trace
-        fig.add_trace(
-            go.Scatter(
-                x=x_sub,
-                y=p_sub(x_sub_num),
-                name='trend',
-                mode='lines',
-                opacity=1,
-                line=dict(color='green',
-                          width=4,
-                          dash='dash')
-            ))
-        # adding the 2 week's moving avg trace
-        fig.add_trace(
-            go.Scatter(
-                x=df.date,
-                y=df[var_new+'_ma'+str(14)],
-                name=var_new+'_ma'+str(14),
-                mode='lines',
-                opacity=1,
-                line=dict(color='red',
-                          width=4),
-                showlegend=True
-            ))
-        # adding the smoothed trace
-        fig.add_trace(
-            go.Scatter(
-                x=df.date,
-                y=df[var_new+'_smooth'],
-                name='smoothed',
-                mode='lines',
-                opacity=1,
-                line=dict(color='purple',
-                          width=6),
-                showlegend=True
-            ))
-        fig.update_layout(
-            xaxis={'title': None,
-                   'titlefont': {'color': '#BFBFBF',
-                                 'family': font_use},
-                   'tickfont': {'color': '#002A34',
-                                'size': 30,
-                                'family': font_use},
-                   'gridcolor': '#4A4A4A',
-                   'linecolor': '#000000',
-                   'showgrid': False},
-            yaxis={'title': 'Digital Demand',
-                   'titlefont': {'color': '#002A34',
-                                 'size': 50,
-                                 'family': font_use},
-                   'tickfont': {'color': '#002A34',
-                                'size': 30,
-                                'family': font_use},
-                   'showgrid': False,
-                   'zeroline': False},
-            margin={'l': 170,
-                    'b': 150,
-                    't': 150,
-                    'r': 40},
-            title={'text': f'Digital Demand for {", ".join(texts).capitalize()}',
-                   'font': {'color': '#000000',
-                            'size': 40,
-                            'family': font_use},
-                   'yanchor': "top",
-                   'xanchor': "center"},
-            legend={'font': {'size': 20,
-                             'color': '#333',
+    fig.add_trace(
+        go.Scatter(
+            x=df.date,
+            y=df[var_new],
+            name='original',
+            mode='lines',
+            opacity=0.3,
+            line=dict(color='#024D83',
+                      width=4),
+            showlegend=True
+        ))
+    # creating the trendline values
+    df_trend = df[['date', var_new]]  # i.e we need date and vl_value
+    # dropping 0 because trendlines can't cope without numeric values
+    df_trend0 = df_trend.dropna()
+    x_sub = df_trend0.date
+    y_sub = df_trend0[var_new]
+    # transforming dates to numeric values, necessary for polynomial fitting
+    x_sub_num = mdates.date2num(x_sub)
+    z_sub = np.polyfit(x_sub_num, y_sub, 1)  # polynomial fitting
+    p_sub = np.poly1d(z_sub)
+    # adding the trendline trace
+    fig.add_trace(
+        go.Scatter(
+            x=x_sub,
+            y=p_sub(x_sub_num),
+            name='trend',
+            mode='lines',
+            opacity=1,
+            line=dict(color='green',
+                      width=4,
+                      dash='dash')
+        ))
+    # adding the 2 week's moving avg trace
+    fig.add_trace(
+        go.Scatter(
+            x=df.date,
+            y=df[var_new+'_ma'+str(14)],
+            name=var_new+'_ma'+str(14),
+            mode='lines',
+            opacity=1,
+            line=dict(color='red',
+                      width=4),
+            showlegend=True
+        ))
+    # adding the smoothed trace
+    fig.add_trace(
+        go.Scatter(
+            x=df.date,
+            y=df[var_new+'_smooth'],
+            name='smoothed',
+            mode='lines',
+            opacity=1,
+            line=dict(color='purple',
+                      width=6),
+            showlegend=True
+        ))
+    fig.update_layout(
+        xaxis={'title': None,
+               'titlefont': {'color': '#BFBFBF',
                              'family': font_use},
-                    'yanchor': "top",
-                    'xanchor': "center",
-                    'y': 0.9,
-                    'x': .95,
-                    'orientation': 'v'},
-            template='none',
-            hovermode='x unified',
-            # width=1920,
-            # height=1080,
+               'tickfont': {'color': '#002A34',
+                            'size': 30,
+                            'family': font_use},
+               'gridcolor': '#4A4A4A',
+               'linecolor': '#000000',
+               'showgrid': False},
+        yaxis={'title': 'Digital Demand',
+               'titlefont': {'color': '#002A34',
+                             'size': 50,
+                             'family': font_use},
+               'tickfont': {'color': '#002A34',
+                            'size': 30,
+                            'family': font_use},
+               'showgrid': False,
+               'zeroline': False},
+        margin={'l': 170,
+                'b': 150,
+                't': 150,
+                'r': 40},
+        title={'text': f'{text}'.capitalize(),
+               'font': {'color': '#000000',
+                        'size': 40,
+                        'family': font_use},
+               'yanchor': "top",
+               'xanchor': "center"},
+        legend={'font': {'size': 20,
+                         'color': '#333',
+                         'family': font_use},
+                'yanchor': "top",
+                'xanchor': "center",
+                'y': 0.9,
+                'x': .95,
+                'orientation': 'v',
+                },
+        template='none',
+        hovermode='x unified',
+        width=1920,
+        height=1080
+    )
 
-        )
-
-        # write image
-        if out_type == 'svg':
-            fig.write_image(f"{text}.{output_type}")
-        elif out_type == 'html':
-            fig.write_html(f"{text}.{output_type}")
-        else:
-            fig.write_image(f"{text}.{output_type}")
-        print(fig)
-        return fig.to_html()
+    # write image
+    if out_type == 'svg':
+        fig.write_image(f"{text}.{output_type}")
+    elif out_type == 'html':
+        fig.write_html(f"{text}.{output_type}")
+    else:
+        fig.write_image(f"{text}.{output_type}")
+    print(fig)
+    return fig.to_html()
 
 
 @app.route('/')
 def index():
     # Call your single function here or modify the code accordingly
     fig_html = single(
-        texts=texts,
         key=f'{text.lower()}',
         geo='DE',
         cat=13,
@@ -259,27 +251,6 @@ def index():
 
     # Render the HTML template to display the chart
     return render_template('index.html', fig_html=fig_html)
-
-
-@app.route('/generate_graph')
-def generate_graph():
-    # Get the user-entered text from the query parameter
-    text = request.args.get('text')
-
-    # Call the single function with the entered text and other parameters
-    fig_html = single(
-        key=text.lower(),
-        geo='DE',
-        cat=13,
-        startdate=init_date,
-        index=True,
-        indexdate=index_date,
-        font_use='Roboto Mono Light for Powerline',
-        out_type=output_type
-    )
-
-    # Render the HTML template to display the new graph
-    return render_template('graph.html', fig_html=fig_html)
 
 
 if __name__ == '__main__':
